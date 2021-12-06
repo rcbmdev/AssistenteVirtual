29bef665f5c470f0edca15c10828cad043427144
Rodrigo <rcbm539@gmail.com>
2021-12-06 12:01:12 -0300

    Adicionando funcionalidades de comandos do CMD

--- 10 - AssistenteVirtual.py
+++ 10 - AssistenteVirtual.py
@@ -3,6 +3,7 @@ from gtts import gTTS
 from playsound import playsound
 import os
 import sys
+from datetime import datetime
 
 tts = gTTS("Olá me chamo Maria. Em que posso ajudá-lo", lang="pt-BR")
 tts.save("audios/welcome.mp3")
@@ -34,8 +35,19 @@ def monitora_audio():
     return trigger
 
 def executa_comandos(trigger):
-    if 'dia' in trigger:
-        cria_audio("Hoje é domingo")
+    # Comandos CMD
+    if 'horas' in trigger:
+        hora = datetime.now().strftime("%H:%M")
+        mensagem = f'São exatamente{hora}'
+        cria_audio(mensagem)
+    elif 'desligar o computador' and 'uma hora' in trigger:
+        os.system("shutdown -s -t 3600")
+    elif 'desligar o computador' and 'meia hora' in trigger:
+        os.system("shutdown -s -t 1800")
+    elif 'cancelar desligamento' in trigger:
+        os.system("shutdown -a")
+    elif 'jupyter notebook' in trigger:
+        os.system("jupyter notebook")
     elif 'fechar assistente' in trigger:
         sys.exit()
 
